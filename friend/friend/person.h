10042da1f977d95c40a214b7999aa85e18d6543b
N了个B <894892935@qq.com>
2016-04-15 10:10:52 +0800

    tianjin

---
+++ friend/friend/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  friend
+//
+//  Created by ljw on 16/4/15.
+//  Copyright © 2016年 ljw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface person : LJWTestCollectionViewController
+
+@end
---
+++ friend/friend/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  friend
+//
+//  Created by ljw on 16/4/15.
+//  Copyright © 2016年 ljw. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
