d672f9f26d83f31282c9c6d3b2a3831d8ff633a5
N了个B <894892935@qq.com>
2016-04-15 10:21:25 +0800

    tianjiaxiangmu

---
+++ WEIXIN/WEIXIN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WEIXIN
+//
+//  Created by ljw on 16/4/14.
+//  Copyright © 2016年 ljw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WEIXIN/WEIXIN/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WEIXIN
+//
+//  Created by ljw on 16/4/14.
+//  Copyright © 2016年 ljw. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
